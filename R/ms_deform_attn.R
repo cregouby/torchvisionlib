53a6221662533f48d1b58f723c9d772661a366ef
C. Regouby <christophe.regouby@free.fr>
2025-11-10 11:21:48 +0100

    document and fix example

--- R/ms_deform_attn.R
+++ R/ms_deform_attn.R
@@ -5,7 +5,7 @@
 #' @param value (N, sum(H_l * W_l), C) input tensor
 #' @param spatial_shapes (L, 2) tensor of feature map heights and widths
 #' @param level_start_index (L,) long tensor, starting index for each level
-#' @param sampling_loc (N, Lq, L, P, 2) normalized [0,1] sampling coordinates
+#' @param sampling_loc (N, Lq, L, P, 2) normalized \[0,1\] sampling coordinates
 #' @param attn_weight (N, Lq, L, P) attention weights
 #' @param im2col_step (int) number of samples processed per kernel launch (tune for memory)
 #'
@@ -16,12 +16,13 @@
 #' custom CUDA kernels.
 #'
 #' @examples
-#' if (torch::torch_cuda_is_available()) {
-#'   value <- torch::torch_randn(1, 100, 256)$to(device = "cuda")
-#'   spatial_shapes <- torch::torch_tensor(matrix(c(10,10, 5,5), ncol=2, byrow=TRUE), dtype = torch::torch_long())$to(device = "cuda")
-#'   level_start_index <- torch::torch_tensor(c(0, 100), dtype = torch::torch_long())$to(device = "cuda")
-#'   sampling_loc <- torch::torch_rand(1, 10, 2, 4, 2)$to(device = "cuda")
-#'   attn_weight <- torch::torch_rand(1, 10, 2, 4)$to(device = "cuda") %>% torch::torch_softmax(dim=3)
+#' if (torch::torch_is_installed()) {
+#'   library(torch)
+#'   value <- torch_randn(1, 100, 256)
+#'   spatial_shapes <- torch_tensor(matrix(c(10,10, 5,5), ncol=2, byrow=TRUE), dtype = torch_long())
+#'   level_start_index <- torch_tensor(c(0, 100), dtype = torch_long())
+#'   sampling_loc <- torch_rand(1, 10, 2, 4, 2)
+#'   attn_weight <- torch_rand(1, 10, 2, 4) %>% nnf_softmax(dim=3)
 #'
 #'   out <- nnf_multiscale_deformable_attn(
 #'     value, spatial_shapes, level_start_index,
